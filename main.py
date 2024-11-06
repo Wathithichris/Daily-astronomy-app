7c5e2735968ae170371f72f18c15360d37fd32db
Chris <mwangichris675@gmail.com>
2024-11-06 13:28:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+import time
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+today = time.strftime("%Y-%m-%d")
+
+api_key = os.getenv("MY_KEY")
+url= "https://api.nasa.gov/planetary/apod?"\
+     f"&date={today}"\
+     f"&api_key={api_key}"
+
+response1 = requests.get(url)
+data = response1.json()
+
+title = data['title']
+img_copyright = data['copyright']
+explanation = data['explanation']
+image_url = data['hdurl']
+response2 = requests.get(image_url)
+
+st.title(title)
+st.image(image_url, caption=f"{title}")
+st.write(explanation)
+st.info(f"Copyright :{img_copyright}  {image_url}")
+
+
+
+
+
